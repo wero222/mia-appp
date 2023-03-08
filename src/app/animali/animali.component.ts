acebf97cccdf7dfd6569ba33c877de720c05abd2
Weronika <weroszymura@gmail.com>
2023-03-08 10:29:47 +0100

    fatto

---
+++ src/app/animali/animali.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animali',
+  templateUrl: './animali.component.html',
+  styleUrls: ['./animali.component.css']
+})
+
+export class AnimaliComponent {
+  titolo: string = "I miei animali";
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnimaliComponent } from './animali/animali.component';
+import { PersoneComponent } from './persone/persone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimaliComponent,
+    PersoneComponent
   ],
   imports: [
     BrowserModule
